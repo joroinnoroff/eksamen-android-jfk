bc4470ca93689dc594dcde5301cc3188565f54fc
kelsolini <kenneth_nygard@outlook.com>
2025-11-26 21:22:12 +0100

    lagt til navbar + screens/viewmodels

---
+++ app/src/main/java/com/example/animee/screens/searchanime/SearchAnimeScreen.kt
@@ -0,0 +1,14 @@
+package com.example.animee.screens.searchanime
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchAnimeScreen(
+    searchAnimeViewModel: SearchAnimeViewModel
+) {
+    Column() {
+        Text("Search anime")
+    }
+}
