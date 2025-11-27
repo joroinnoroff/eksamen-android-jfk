7ad67348b49fb824aedc73ad03fc34085ca211d2
kelsolini <kenneth_nygard@outlook.com>
2025-11-27 23:00:40 +0100

    new anime detail

---
+++ app/src/main/java/com/example/animee/components/NewAnimeItem.kt
@@ -0,0 +1,33 @@
+package com.example.animee.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.animee.data.room.NewAnime
+
+@Composable
+fun NewAnimeItem(
+    newAnime: NewAnime,
+    editAnime: () -> Unit,
+    deleteAnime: () -> Unit
+){
+    Column {
+        Text(newAnime.title)
+        Text(newAnime.description.toString())
+
+        Row() {
+            Button(
+                editAnime
+            ) {
+                Text("Rediger")
+            }
+            Button(
+                deleteAnime
+            ) {
+                Text("Slett")
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/animee/screens/animecreate/AnimeCreateScreen.kt
+++ app/src/main/java/com/example/animee/screens/animecreate/AnimeCreateScreen.kt
@@ -1,15 +1,77 @@
 package com.example.animee.screens.animecreate
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.navigation.NavController
+import com.example.animee.components.NewAnimeListItem
+import com.example.animee.data.room.NewAnime
+import com.example.animee.navigation.NavRoutes
 
 
 @Composable
 
-fun AnimeCreateScreen(animeCreateViewModel: AnimeCreateViewModel) {
-    var title by remember { mutableStateOf("") }
+fun AnimeCreateScreen(
+    animeCreateViewModel: AnimeCreateViewModel,
+    navController: NavController
+    ) {
+    val newAnimes = animeCreateViewModel.newAnimes.collectAsState()
+
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
+    Column() {
+        Text("Ny anime")
+        TextField(
+            value = title,
+            onValueChange = { title = it },
+            label = { Text("Title") }
+        )
+        TextField(
+            value = description,
+            onValueChange = { description = it },
+            label = { Text("Description") }
+        )
+        Button(
+            onClick = {
+                if (title.isNotEmpty() && description.isNotEmpty()) {
+                    animeCreateViewModel.insertNewAnime(
+                        NewAnime(title = title, description = description)
+                    )
+                }
+            }
+        ) {
+            Text("Legg til ny anime")
+        }
+
+        if (newAnimes.value.count() > 0) {
+            LazyColumn {
+                items(newAnimes.value) { anime->
+                    NewAnimeListItem(
+                        anime,
+                        editAnime = {
+                            navController.navigate(NavRoutes.AnimeCreateDetailsRoute(anime.id))
+                        }
+                    )
+                }
+            }
+        } else {
+            Text("Ingen nye animer i databasen")
+        }
+    }
 }
 
