4d99ffc31003a5ca527bc86c99fddb74b81cffe9
Fabian <fabi001@egms.no>
2025-11-28 12:44:12 +0100

    jobber videre med layout på hjem side, mulig noe må endres for å integrere flow? idk, men det finner man ut av

--- app/src/main/java/com/example/animee/components/SimpleAnimeItem.kt
+++ app/src/main/java/com/example/animee/components/SimpleAnimeItem.kt
@@ -1,24 +1,44 @@
 package com.example.animee.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.animee.data.retrofit.AnimeApi
 
 @Composable
 fun SimpleAnimeItem(anime: AnimeApi){
-    Column() {
-        Text(anime.englishTitle)
+    Box(modifier = Modifier
+        .width(300.dp)
+        .clip(RoundedCornerShape(16.dp))
+        ){
+            AsyncImage(
+                model = anime.images.webp.image,
+                contentDescription = anime.englishTitle,
+                modifier = Modifier.fillMaxWidth()
+                    .height(300.dp)
+            )
+            Box(
+                modifier = Modifier.fillMaxWidth()
 
-        AsyncImage(
-            model = anime.images.webp.image,
-            contentDescription = anime.englishTitle
-        )
-        Row() {
-            Text(anime.score.toString())
-            Text(anime.popularity.toString())
+            ){
+                Column() {
+                    Text(anime.englishTitle)
+                    Row() {
+                        Text(anime.score.toString())
+                        Text(anime.popularity.toString())
+                    }
+                }
         }
     }
 }
