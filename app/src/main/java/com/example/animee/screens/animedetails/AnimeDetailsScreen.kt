727bcf744d97814ddd5edc3b7923f542e943a15e
joroinnoroff <oinojorgen@gmail.com>
2025-11-28 18:54:00 +0100

    animeApiDetails page

--- app/src/main/java/com/example/animee/screens/animedetails/AnimeDetailsScreen.kt
+++ app/src/main/java/com/example/animee/screens/animedetails/AnimeDetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.animee.screens.animedetails
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,18 +17,22 @@ fun AnimeDetailsScreen(
 ) {
     val anime = animeDetailsViewModel.anime.collectAsState()
 
-//    LaunchedEffect(Unit) {
-//        animeDetailsViewModel.setAnime(animeId)
-//    }
+    LaunchedEffect(animeId) {
+        animeDetailsViewModel.setAnimeDetails(animeId)
+    }
 
     Column() {
         Text("Anime detail screen")
-        anime.value?.let {
-            AnimeItem(it,
-                goBack = {
-                    navController.popBackStack()
-                }
-            )
+
+
+        LazyColumn() {
+            item {  anime.value?.let {
+                AnimeItem(it,
+                    goBack = {
+                        navController.popBackStack()
+                    }
+                )
+            } }
         }
     }
 }
